c0e17bce26b586c94b223818340f59d070be2154
Szokol Attila <szokol.ati2005@gmail.com>
2022-05-29 11:54:33 +0200

    szerintem kész :>

--- RPS-AI/HandPose/Hands.py
+++ RPS-AI/HandPose/Hands.py
@@ -6,13 +6,50 @@ from cv2 import cv2
 import hand_detection_module
 from data_generation import num_hand
 import pickle
+import time
+import threading
 
 model_name = 'hand_model.sav'
+
+def points():
+  for i in range(1, 4):
+    time.sleep(1)
+    print(4-i)
+  p1.append(p1pred)
+  p2.append(p2pred)
 
 def rps(num):
   if num == 0: return 'PAPER'
   elif num == 1: return 'ROCK'
-  else: return 'SCISSOR'
+  else: return 'SCISSORS'
+
+def defineWinner(p1, p2):
+  p1points = 0
+  p2points = 0
+  for i in range(len(p1)):
+    if p1[i] == "SCISSORS":
+      if p2[i] == "PAPER":
+        p1points += 1
+      elif p2[i] == "ROCK":
+        p2points += 1
+    elif p1[i] == "ROCK":
+      if p2[i] == "PAPER":
+        p2points += 1
+      elif p2[i] == "SCISSORS":
+        p1points += 1
+    else:
+      if p2[i] == "ROCK":
+        p1points += 1
+      elif p2[i] == "SCISSORS":
+        p2points += 1
+  if p1points > p2points:
+    winner = "the winner is: P1"
+  elif p2points > p1points:
+    winner = "the winner is: P2"
+  else:
+    winner = "it's a draw"
+  return f"P1: {p1points}, P2: {p2points}, {winner}"
+
 
 class mpHands:
     import mediapipe as mp
@@ -34,6 +71,8 @@ class mpHands:
                 myHands.append(myHand)
         return myHands,handsType
 
+p1 = []
+p2 = []
 font = cv2.FONT_HERSHEY_PLAIN
 hands = hand_detection_module.HandDetector(max_hands = num_hand)
 model = pickle.load(open(model_name,'rb'))
@@ -62,8 +101,18 @@ with mp_hands.Hands(
       pred = rps(model.predict([all_distance])[0])
       pos = (int(hand[12][0]*height), int(hand[12][1]*width))
       pos2 = (int(hand[12][0]*height), int(hand[12][1]*width))
-      image = cv2.putText(image,pred,pos,font,2,(0,0,0),2)
+      if my_list.index(hand) == 0:
+        player = "P1:"
+        p1pred = pred
+      else:
+        player = "P2:"
+        p2pred = pred
+      image = cv2.putText(image,player + pred,pos,font,2,(0,0,0),2)
+    countDown = threading.Thread(target=points)
+    if cv2.waitKey(1) == ord('c'):
+      countDown.start()
     if cv2.waitKey(1) == ord('x'):
+      print(defineWinner(p1, p2))
       break
     cv2.imshow('MediaPipe Hands', image)
 cap.release()
